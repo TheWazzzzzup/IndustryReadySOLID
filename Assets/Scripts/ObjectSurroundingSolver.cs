f113ec2a4c35704a896716d0bd0112e9253519cc
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-07-27 22:31:24 +0200

    Import TMP

--- Assets/Scripts/ObjectSurroundingSolver.cs
+++ Assets/Scripts/ObjectSurroundingSolver.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.TextCore.LowLevel;
 
 [RequireComponent(typeof(ICheckSurrounding))]
 public class ObjectSurroundingSolver : MonoBehaviour , IHandleSurroundingInteractions
 {
     ICheckSurrounding surroundingChecker;
-  
+
+    GameObject firstInteractedObject;
+
     private void Awake()
     {
         surroundingChecker = GetComponent<ICheckSurrounding>();
@@ -16,10 +18,7 @@ public class ObjectSurroundingSolver : MonoBehaviour , IHandleSurroundingInterac
     public void HandleSurroundingInteraction()
     {
         surroundingChecker.CheckSurroundings();
-        foreach (GameObject go in surroundingChecker.surroundingGO)
-        {
-            // Add Action to the game object
-        }
+        // Todo : enter logic of the player
     }
 
 }
